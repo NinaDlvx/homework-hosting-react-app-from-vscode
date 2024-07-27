a402be2951c386c307c5690c9a9e3cdcd9cdf963
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-07-27 17:33:53 +0200

    Added footer with links

--- src/index.js
+++ src/index.js
@@ -14,6 +14,31 @@ root.render(
       <h1>Weather App</h1>
       <WeatherSearch />
       <img src={logo} className="App-logo" alt="logo of react" />
+      <footer>
+        This Project was coded by
+        <a href="https://github.com/NinaDlvx" target="_blank" rel="noreferrer">
+          Nina Delvaux
+        </a>
+        ,
+        <br />
+        is
+        <a
+          href="https://github.com/NinaDlvx/homework-hosting-react-app-from-vscode"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+        and
+        <a
+          href="https://magenta-sopapillas-7908e7.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+        .
+      </footer>
     </div>
   </StrictMode>
 );
